0eda1094667c11d1db5e67006ce330efaba546b4
Austin Derrow-Pinion <derrowap@rose-hulman.edu>
2016-06-24 15:20:50 -0400

    finished second algorithm

--- app/controllers/trominoes.js
+++ app/controllers/trominoes.js
@@ -2,14 +2,17 @@
 
 angular.module('app.trominoes', []).
 controller('trominoesCtrl', function($scope) {
-	var trominoesCtrl = this;
-	$scope.Math = window.Math;
-	$scope.boardSize = 4;
-	this.squareSize = 30;
-	var movesDrawn = 0;
-	this.moves = [];
-	this.currentOrientation = 'topLeft';
+	var trominoesCtrl = this; // allows functions to reference this controller's variables
+	$scope.Math = window.Math; // allows angular expressions to use this now
+	$scope.boardSize = 4; // size of the board in a power of two
+	this.squareSize = 30; // pixel size of every square
+	var movesDrawn = 0; // number of moves drawn on the board
+	this.moves = []; // stores all of the moves in the order they were pushed
+	this.currentOrientation = 'single'; // current orientation of a tile if a user clicked on the board
+	this.speed = 2000 // controls the speed of solving. 2000 is fastest, 0 is slowest.
 
+	// Data about all of the 4 different tromino orientations and the missing
+	// square (or single) piece.
 	this.trominoes = {
 		'single': {
 			'0': {
@@ -154,6 +157,10 @@ controller('trominoesCtrl', function($scope) {
 
 
 	$scope.setupBoard = function () {
+		/*
+		Initializes a 2^n x 2^n deficient board. Called every time a new
+		board needs to be displayed.
+		*/
 		trominoesCtrl.board = [];
 		for (var i = 0; i < Math.pow(2, $scope.boardSize); i++) {
 			trominoesCtrl.board[i] = [];
@@ -173,7 +180,9 @@ controller('trominoesCtrl', function($scope) {
 		}
 	}
 
-	$scope.setupBoard();
+	$scope.setupBoard(); // sets the board up on first load
+
+	// watches for when a user wants to change the size of the squares
 	$scope.$watch(function () {return trominoesCtrl.squareSize;},
 		function () {
 			for (var i = 0; i < trominoesCtrl.board.length; i++) {
@@ -186,33 +195,44 @@ controller('trominoesCtrl', function($scope) {
 	);
 
 	$scope.restartGame = function() {
+		/*
+		Restarts the game by re-initializing all the variables in a game
+		instance. Called immediately when the Restart button on UI is pressed.
+		*/
 		movesDrawn = 0;
 		trominoesCtrl.moves = [];
+		$scope.error = null;
 		$scope.setupBoard();
 	};
 
 	$scope.updateBoardSize = function(increase) {
-		if (increase && $scope.boardSize < 5) {
+		/*
+		Increases or decreases the size of the board. Limits the possible
+		sizes to between 2^1 x 2^1 to 2^6 x 2^6, or 2x2 to 64x64.
+		*/
+		if (increase && $scope.boardSize < 6) {
 			$scope.boardSize++;
-			$scope.error = null;
-			trominoesCtrl.moves = [];
-			$scope.setupBoard();
+			$scope.restartGame();
 		} else if (!increase && $scope.boardSize > 1) {
 			$scope.boardSize--;
-			$scope.error = null;
-			trominoesCtrl.moves = [];
-			$scope.setupBoard();
+			$scope.restartGame();
 		} else {
-			$scope.error = "Allowed range for size of board is 2 to 32.";
+			$scope.error = "Allowed range for size of board is 2 to 64.";
 		}
 	};
 
 	$scope.changeOrientation = function(orientation) {
+		/*
+		Changes the current orientation to the input. Called from the UI
+		when a user selects an orientation on the radio inputs.
+		*/
 		trominoesCtrl.currentOrientation = orientation;
 	}
 
 	var drawMove = function(index) {
-		console.log("drawMove", index, trominoesCtrl.moves[index]);
+		/*
+		Draws a move in trominoesCtrl.moves at the specified index.
+		*/
 		var move = trominoesCtrl.moves[index];
 		var trominoPiece = trominoesCtrl.trominoes[move.orientation];
 
@@ -225,10 +245,15 @@ controller('trominoesCtrl', function($scope) {
 				trominoesCtrl.board[move.y + spot.yOffset][move.x + spot.xOffset].used = true;
 			}
 		});
-		
 	};
 
 	$scope.pushMove = function(col, row) {
+		/*
+		Pushes a move at the given col and row position with the current 
+		orientation. This method is only used when a user is placing tiles by
+		themself via clicking on the board. Will throw an error if it is an
+		invalid move.
+		*/
 
 		// Check if this is a valid move
 		var trominoPiece = trominoesCtrl.trominoes[trominoesCtrl.currentOrientation];
@@ -240,7 +265,7 @@ controller('trominoesCtrl', function($scope) {
 				|| (x >= Math.pow(2, $scope.boardSize))
 				|| (y < 0)
 				|| (y >= Math.pow(2, $scope.boardSize))
-				|| (trominoesCtrl.board[x][y].used)) {
+				|| (trominoesCtrl.board[y][x].used)) {
 				$scope.error = "That tile does not fit here!";
 				return;
 			} 
@@ -261,7 +286,6 @@ controller('trominoesCtrl', function($scope) {
 			'y': row,
 			'orientation': trominoesCtrl.currentOrientation
 		});
-		console.log("updated moves:", trominoesCtrl.moves);
 
 		// draw move
 		drawMove(trominoesCtrl.moves.length - 1);
@@ -269,9 +293,14 @@ controller('trominoesCtrl', function($scope) {
 	};
 
 	var baseCase = function(missingTile, missingSection) {
+		/*
+		In the base case when the board is a 2x2 deficient board, this pushes
+		a move of the proper orientation at the proper location to fill the
+		remaining space.
+		*/
 		switch (missingSection) {
 			case 1:
-				// draw bottom left
+				// draw bottom left tromino
 				trominoesCtrl.moves.push({
 					'x': missingTile.col - 1,
 					'y': missingTile.row + 1,
@@ -279,7 +308,7 @@ controller('trominoesCtrl', function($scope) {
 				});
 				break;
 			case 2:
-				// draw bottom right
+				// draw bottom right tromino
 				trominoesCtrl.moves.push({
 					'x': missingTile.col + 1,
 					'y': missingTile.row + 1,
@@ -287,7 +316,7 @@ controller('trominoesCtrl', function($scope) {
 				});
 				break;
 			case 3:
-				// draw top right
+				// draw top right tromino
 				trominoesCtrl.moves.push({
 					'x': missingTile.col + 1,
 					'y': missingTile.row - 1,
@@ -295,19 +324,21 @@ controller('trominoesCtrl', function($scope) {
 				});
 				break;
 			case 4:
-				// draw top left
+				// draw top left tromino
 				trominoesCtrl.moves.push({
 					'x': missingTile.col - 1,
 					'y': missingTile.row - 1,
 					'orientation': 'topLeft'
 				});
-				break;
 		}
 	};
 
 	var jHelper = function(missingTile, xStart, xEnd, yStart, yEnd) {
+		/*
+		Implements the divide and conquer algorithm to tile a 2^n x 2^n 
+		deficient board as described in the Johnsonbaugh paper.
+		*/
 		var missingSection = whichSection(missingTile, xStart, xEnd, yStart, yEnd);
-		console.log("jHelper:", missingTile, xStart, xEnd, yStart, yEnd, missingSection);
 		if (xEnd - xStart == 1) {
 			// Base case
 			baseCase(missingTile, missingSection);
@@ -315,9 +346,10 @@ controller('trominoesCtrl', function($scope) {
 		}
 		switch (missingSection) {
 			case 1:
+				// missing square is in top right section
 				// draw bottom left tromino
-				var newX = Math.floor((xEnd - xStart) / 2) + xStart;
-				var newY = Math.floor((yEnd - yStart) / 2) + yStart + 1;
+				var newX = Math.floor((xEnd + xStart) / 2);
+				var newY = Math.floor((yEnd + yStart) / 2) + 1;
 				trominoesCtrl.moves.push({
 					'x': newX,
 					'y': newY,
@@ -329,9 +361,10 @@ controller('trominoesCtrl', function($scope) {
 				jHelper({'col': newX, 'row': newY - 1}, xStart, newX, yStart, newY - 1); // top left
 				break;
 			case 2:
+				// missing square is in the top left section
 				// draw bottom right tromino
-				var newX = Math.floor((xEnd - xStart) / 2) + xStart + 1;
-				var newY = Math.floor((yEnd - yStart) / 2) + yStart + 1;
+				var newX = Math.floor((xEnd + xStart) / 2) + 1;
+				var newY = Math.floor((yEnd + yStart) / 2) + 1;
 				trominoesCtrl.moves.push({
 					'x': newX,
 					'y': newY,
@@ -343,9 +376,10 @@ controller('trominoesCtrl', function($scope) {
 				jHelper({'col': newX - 1, 'row': newY}, xStart, newX - 1, newY, yEnd); // bottom left
 				break;
 			case 3:
+				// missing square is in the bottom left section
 				// draw top right tromino
-				var newX = Math.floor((xEnd - xStart) / 2) + xStart + 1;
-				var newY = Math.floor((yEnd - yStart) / 2) + yStart;
+				var newX = Math.floor((xEnd + xStart) / 2) + 1;
+				var newY = Math.floor((yEnd + yStart) / 2);
 				trominoesCtrl.moves.push({
 					'x': newX,
 					'y': newY,
@@ -357,9 +391,10 @@ controller('trominoesCtrl', function($scope) {
 				jHelper({'col': newX, 'row': newY + 1}, newX, xEnd, newY + 1, yEnd); // bottom right
 				break;
 			case 4:
+				// missing square is in the bottom right section
 				// draw top left tromino
-				var newX = Math.floor((xEnd - xStart) / 2) + xStart;
-				var newY = Math.floor((yEnd - yStart) / 2) + yStart;
+				var newX = Math.floor((xEnd + xStart) / 2);
+				var newY = Math.floor((yEnd + yStart) / 2);
 				trominoesCtrl.moves.push({
 					'x': newX,
 					'y': newY,
@@ -369,45 +404,28 @@ controller('trominoesCtrl', function($scope) {
 				jHelper({'col': newX, 'row': newY + 1}, xStart, newX, newY + 1, yEnd); // bottom left
 				jHelper({'col': newX, 'row': newY}, xStart, newX, yStart, newY); // top left
 				jHelper({'col': newX + 1, 'row': newY}, newX + 1, xEnd, yStart, newY); // top right
-				break;
 		}
 	}
 
 	var whichSection = function(missingTile, xStart, xEnd, yStart, yEnd) {
-		/* Helper function returns which section the missing tile is in, labeled
+		/* 
+		Helper function returns which section the missing tile is in, labeled
 		the same as a Cartesian Plane, such that top right is 1, top left is 2,
-		bottom left is 3, and bottom right is 4. */
-		// console.log(missingTile, xStart, xEnd, yStart, yEnd);
-		if (missingTile.col <= Math.floor((xEnd - xStart) / 2) + xStart) {
-			return (missingTile.row <= Math.floor((yEnd - yStart) / 2) + yStart) ? 2 : 3;
+		bottom left is 3, and bottom right is 4.
+		*/
+		if (missingTile.col <= Math.floor((xEnd + xStart) / 2)) {
+			return (missingTile.row <= Math.floor((yEnd + yStart) / 2)) ? 2 : 3;
 		} else {
-			return (missingTile.row <= Math.floor((yEnd - yStart) / 2) + yStart) ? 1 : 4;
+			return (missingTile.row <= Math.floor((yEnd + yStart) / 2)) ? 1 : 4;
 		}
 	}
 
-	/* Johnsonbaugh Algorithm */
-	this.tileJohnsonbaugh = function() {
-		console.log("You chose the Johnsonbaugh algorithm.");
-		var missingTile = {
-			'col': trominoesCtrl.moves[0].x,
-			'row': trominoesCtrl.moves[0].y
-		};
-		console.log("missing tile", missingTile);
-		var xStart = 0;
-		var xEnd = Math.pow(2, $scope.boardSize) - 1;
-		var yStart = 0;
-		var yEnd = Math.pow(2, $scope.boardSize) - 1;
-		console.log("section", whichSection(missingTile, xStart, xEnd, yStart, yEnd));
-		jHelper(missingTile, xStart, xEnd, yStart, yEnd);
-		
-		// draw moves
-		trominoesCtrl.drawInterval = setInterval(drawNextMove, 2000 - trominoesCtrl.speed);
-
-		console.log("finished drawing moves");
-	};
-
 	function drawNextMove() {
-		console.log("drawNextMove", movesDrawn, trominoesCtrl.moves.length);
+		/*
+		Draws the next move at index movesDrawn. Called in a user specified
+		interval and when all the moves are drawn, it stops the interval from
+		executing any further.
+		*/
 		if (trominoesCtrl.moves.length <= movesDrawn) {
 			// all moves have been drawn
 			clearInterval(trominoesCtrl.drawInterval);
@@ -417,10 +435,116 @@ controller('trominoesCtrl', function($scope) {
 		movesDrawn++;
 	}
 
-	/* L-Recursive Algorithm */
-	this.tileL = function() {
-		console.log("You chose the L-Recursive algorithm.");
+	var tileLShape = function (xStart, xEnd, yStart, yEnd, orientation) {
+		// Check for base case of L-Shape size of 1 tromino
+		if (xEnd - xStart == 1) {
+			switch (orientation) {
+				case 'topRight':
+					baseCase({'col': xStart, 'row': yEnd}, 3);
+					break;
+				case 'topLeft':
+					baseCase({'col': xEnd, 'row': yEnd}, 4);
+					break;
+				case 'bottomLeft':
+					baseCase({'col': xEnd, 'row': yStart}, 1);
+					break;
+				case 'bottomRight':
+					baseCase({'col': xStart, 'row': yStart}, 2);
+			}
+			return;
+		}
 
+		switch (orientation) {
+			case 'topRight':
+				tileLShape(Math.ceil((3 * xStart + xEnd) / 4), Math.floor(3 * (xEnd - xStart) / 4) + xStart, Math.ceil((3 * yStart + yEnd) / 4), Math.floor(3 * (yEnd - yStart) / 4) + yStart, 'topRight');
+				tileLShape(xStart, Math.floor((xStart + xEnd) / 2), yStart, Math.floor((yStart + yEnd) / 2), 'topLeft');
+				tileLShape(Math.floor((xStart + xEnd) / 2) + 1, xEnd, yStart, Math.floor((yStart + yEnd) / 2), 'topRight');
+				tileLShape(Math.floor((xStart + xEnd) / 2) + 1, xEnd, Math.floor((yStart + yEnd) / 2) + 1, yEnd, 'bottomRight');
+				break;
+			case 'topLeft':
+				tileLShape(Math.ceil((3 * xStart + xEnd) / 4), Math.floor(3 * (xEnd - xStart) / 4) + xStart, Math.ceil((3 * yStart + yEnd) / 4), Math.floor(3 * (yEnd - yStart) / 4) + yStart, 'topLeft');
+				tileLShape(xStart, Math.floor((xStart + xEnd) / 2), Math.floor((yStart + yEnd) / 2) + 1, yEnd, 'bottomLeft');
+				tileLShape(xStart, Math.floor((xStart + xEnd) / 2), yStart, Math.floor((yStart + yEnd) / 2), 'topLeft');
+				tileLShape(Math.floor((xStart + xEnd) / 2) + 1, xEnd, yStart, Math.floor((yStart + yEnd) / 2), 'topRight');
+				break;
+			case 'bottomLeft':
+				tileLShape(Math.ceil((3 * xStart + xEnd) / 4), Math.floor(3 * (xEnd - xStart) / 4) + xStart, Math.ceil((3 * yStart + yEnd) / 4), Math.floor(3 * (yEnd - yStart) / 4) + yStart, 'bottomLeft');
+				tileLShape(Math.floor((xStart + xEnd) / 2) + 1, xEnd, Math.floor((yStart + yEnd) / 2) + 1, yEnd, 'bottomRight');
+				tileLShape(xStart, Math.floor((xStart + xEnd) / 2), Math.floor((yStart + yEnd) / 2) + 1, yEnd, 'bottomLeft');
+				tileLShape(xStart, Math.floor((xStart + xEnd) / 2), yStart, Math.floor((yStart + yEnd) / 2), 'topLeft');
+				break;
+			case 'bottomRight':
+				tileLShape(Math.ceil((3 * xStart + xEnd) / 4), Math.floor(3 * (xEnd - xStart) / 4) + xStart, Math.ceil((3 * yStart + yEnd) / 4), Math.floor(3 * (yEnd - yStart) / 4) + yStart, 'bottomRight');
+				tileLShape(Math.floor((xStart + xEnd) / 2) + 1, xEnd, yStart, Math.floor((yStart + yEnd) / 2), 'topRight');
+				tileLShape(Math.floor((xStart + xEnd) / 2) + 1, xEnd, Math.floor((yStart + yEnd) / 2) + 1, yEnd, 'bottomRight');
+				tileLShape(xStart, Math.floor((xStart + xEnd) / 2), Math.floor((yStart + yEnd) / 2) + 1, yEnd, 'bottomLeft');
+		}
+	}
+
+	var lHelper = function (missingTile, xStart, xEnd, yStart, yEnd) {
+		console.log("lHelper", missingTile, xStart, xEnd, yStart, yEnd);
+		if (xStart == xEnd) return; // base case when it is just the missing tile;
+		var missingSection = whichSection(missingTile, xStart, xEnd, yStart, yEnd);
+		var orientation = '';
+		switch (missingSection) {
+			case 1:
+				// make deficient board top right, L-Shape bottom left
+				lHelper(missingTile, Math.floor((xEnd + xStart) / 2) + 1, xEnd, yStart, Math.floor((yEnd + yStart) / 2));
+				orientation = 'bottomLeft';
+				break;
+			case 2:
+				// make deficient board top left, L-Shape bottom right
+				lHelper(missingTile, xStart, Math.floor((xEnd + xStart) / 2), yStart, Math.floor((yEnd + yStart) / 2));
+				orientation = 'bottomRight';
+				break;
+			case 3:
+				// make deficient board bottom left, L-Shape top right
+				lHelper(missingTile, xStart, Math.floor((xEnd + xStart) / 2), Math.floor((yEnd + yStart) / 2) + 1, yEnd);
+				orientation = 'topRight';
+				break;
+			case 4:
+				// make deficient board bottom right, L-Shape top left
+				lHelper(missingTile, Math.floor((xEnd + xStart) / 2) + 1, xEnd, Math.floor((yEnd + yStart) / 2) + 1, yEnd);
+				orientation = 'topLeft';
+		}
+		tileLShape(xStart, xEnd, yStart, yEnd, orientation);
 	};
+
+	this.startAlgorithm = function(isLShape) {
+		/*
+		Sets up the data needed to begin either the Johnsonbaugh algorithm or
+		the L-Shape algorithm. Called immediately when one of the buttons are
+		pressed on the UI.
+		*/
+		
+		// check to make sure a missing square has been chosen
+		if (trominoesCtrl.moves.length == 0) {
+			$scope.error = "You first must choose a missing square!";
+			return;
+		}
+
+		// erases all but missing square in case a user first tries to solve
+		var firstMove = trominoesCtrl.moves[0];
+		$scope.restartGame();
+		trominoesCtrl.moves.push(firstMove);
+		drawMove(0);
+
+		var missingTile = {
+			'col': trominoesCtrl.moves[0].x,
+			'row': trominoesCtrl.moves[0].y
+		};
+
+		// begin the algorithm
+		if (isLShape) {
+			console.log("You chose the L-Shape algorithm.");
+			lHelper(missingTile, 0, Math.pow(2, $scope.boardSize) - 1, 0, Math.pow(2, $scope.boardSize) - 1);
+		} else {
+			console.log("You chose the Johnsonbaugh algorithm.");
+			jHelper(missingTile, 0, Math.pow(2, $scope.boardSize) - 1, 0, Math.pow(2, $scope.boardSize) - 1);
+		}
+		
+		// Starts drawing the moves at the user specified speed
+		trominoesCtrl.drawInterval = setInterval(drawNextMove, 2000 - trominoesCtrl.speed);
+	}
 
 });
